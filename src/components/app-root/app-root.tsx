5bc8fc3735edaa03f3c0469c25803d0df1b6923d
Newton Munene <newtonmunene99@gmail.com>
2019-06-25 17:42:56 +0300

    refactor

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -10,7 +10,7 @@ export class AppRoot {
     return (
       <div>
         <header>
-          <h1>Color Palette</h1>
+          <h1>Prominent Colors</h1>
         </header>
 
         <main>
--- stencil.config.ts
+++ stencil.config.ts
@@ -12,7 +12,7 @@ export const config: Config = {
       type: "www",
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: "https://myapp.local/"
+      baseUrl: "https://prominent-colors.netlify.com/"
     }
   ],
   plugins: [sass(), nodePolyfills()]
